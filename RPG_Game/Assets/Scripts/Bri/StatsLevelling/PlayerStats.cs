7521997f1a3619203652b81eda6eebadcd1c5768
gitsbydeerskin <119915520+gitsbydeerskin@users.noreply.github.com>
2025-06-08 12:01:55 +0800

    mod: basic code implementation
    player handler, ui handler, and player stats - not hooked up to any objects yet, not tested, or commented, but the scripts have been set up and SHOULD work once I have access to the terrain and player and UI

--- RPG_Game/Assets/Scripts/Bri/StatsLevelling/PlayerStats.cs
+++ RPG_Game/Assets/Scripts/Bri/StatsLevelling/PlayerStats.cs
@@ -1,16 +1,42 @@
 using UnityEngine;
+using System;
+using System.Collections.Generic;
 
-public class PlayerStats : MonoBehaviour
+public class PlayerStats
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Character Name")]
+    public string name;
+    [Header("Character Attributes")]
+    public Attribute health;
+    public Attribute stamina;
+    [Header("Character Level")]
+    public int level;
+    public Attribute experience;
+    [Header("Character Stats")]
+    public List<Stat> stats = new List<Stat>
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    new Stat { name = "STR", statValue = 12, value = 0 },
+    new Stat { name = "DEX", statValue = 14, value = 0 },
+    new Stat { name = "CON", statValue = 13, value = 0 },
+    new Stat { name = "INT", statValue = 10, value = 0 },
+    new Stat { name = "WIS", statValue = 11, value = 0 },
+    new Stat { name = "CHA", statValue = 8, value = 0 },
+    };
 }
+
+[Serializable]
+public struct Attribute
+{
+    public float currentValue;
+    public float maxValue;
+    public float value;
+}
+
+[Serializable]
+public struct Stat
+{
+    public string name;
+    public int statValue;
+    public int value; 
+}
+
--- RPG_Game/Assets/Scripts/Bri/StatsLevelling/UIManager.cs
+++ RPG_Game/Assets/Scripts/Bri/StatsLevelling/UIManager.cs
@@ -1,16 +1,14 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public Image healthBar;
+    public Image staminaBar;
+    public Image experienceBar;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void UpdateUI(Image displayImage, float currentValue, float maxValue)
+    { 
+        displayImage.fillAmount = Mathf.Clamp01(currentValue / maxValue);
     }
 }
