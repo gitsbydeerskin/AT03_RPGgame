d8b0985ddf32231c39ec9967b47d3ea1041988a1
EthanF11 <ethan.flemming11@gmail.com>
2025-06-09 14:25:33 +0800

    Squashed commit of the following:
    commit 4386514a121a1ba7eb684e50b3f1294e92fb37d7
    Author: gitsbydeerskin <119915520+gitsbydeerskin@users.noreply.github.com>
    Date:   Sun Jun 8 12:56:26 2025 +0800
    
        file change: set up basic Game world
    
        Set up a basic world, terrain, and capsule (Player), spawn point, to test the Stats and Levelling scripts - not functioning right now as there is no HUD components.
    
    commit 7521997f1a3619203652b81eda6eebadcd1c5768
    Author: gitsbydeerskin <119915520+gitsbydeerskin@users.noreply.github.com>
    Date:   Sun Jun 8 12:01:55 2025 +0800
    
        mod: basic code implementation
    
        player handler, ui handler, and player stats - not hooked up to any objects yet, not tested, or commented, but the scripts have been set up and SHOULD work once I have access to the terrain and player and UI
    
    commit fcbc7ca0e1fba61af11b06cdee1964f22575018d
    Author: gitsbydeerskin <119915520+gitsbydeerskin@users.noreply.github.com>
    Date:   Sun Jun 8 11:28:26 2025 +0800
    
        file_change: Stats & Levelling set up
    
        Basic file set up + empty scripts for all the Stats & Levelling scripts - NON-FUNCTIONING
    
    commit 21bf9970a604924d12d0ffda600385c6f626e3bf
    Merge: 6caf6ca 18f8e97
    Author: gitsbydeerskin <119915520+gitsbydeerskin@users.noreply.github.com>
    Date:   Sun Jun 8 11:16:06 2025 +0800
    
        Merge pull request #1 from gitsbydeerskin/feature/Dialogue
    
        mod: folder set up
    
    commit 18f8e97fb36eab25639c18338adede844034f237
    Author: gitsbydeerskin <119915520+gitsbydeerskin@users.noreply.github.com>
    Date:   Sun Jun 8 11:11:47 2025 +0800
    
        mod: folder set up
    
        Minor update - just set up the folder for dialogue scripts

---
+++ RPG_Game/Assets/Scripts/Bri/StatsLevelling/PlayerHandler.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+
+public class PlayerHandler : MonoBehaviour
+{ 
+    public PlayerStats playerData = new PlayerStats();
+
+    [SerializeField] Transform spawnPoint;
+    [SerializeField] float timerValue;
+    [SerializeField] bool canHeal = true;
+
+    public UIManager uiManager;
+
+    public void DamagePlayer(float damageValue)
+    {
+        timerValue = 0;
+        canHeal = false;
+        playerData.health.currentValue -= damageValue;
+        uiManager.UpdateUI(uiManager.healthBar, playerData.health.currentValue, playerData.health.maxValue);
+    }
+
+    void HealOverTime()
+    {
+        if (canHeal)
+        {
+            if (playerData.health.currentValue < playerData.health.maxValue && playerData.health.currentValue > 0)
+            {
+                playerData.health.currentValue += playerData.health.value * Time.deltaTime;
+                uiManager.UpdateUI(uiManager.healthBar, playerData.health.currentValue, playerData.health.maxValue);
+            }
+        }
+    }
+
+    void Timer()
+    {
+        if (!canHeal)
+        {
+            timerValue += Time.deltaTime;
+            if (timerValue > 1.5f)
+            {
+                canHeal = true;
+                timerValue = 0;
+            }
+        }
+    }
+
+    private void Start()
+    {
+        uiManager.UpdateUI(uiManager.healthBar, playerData.health.currentValue, playerData.health.maxValue);
+        uiManager.UpdateUI(uiManager.staminaBar, playerData.stamina.currentValue, playerData.stamina.maxValue);
+        uiManager.UpdateUI(uiManager.experienceBar, playerData.experience.currentValue, playerData.experience.maxValue);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Heal")
+        {
+            playerData.health.value *= 2;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Heal")
+        { 
+            playerData.health.value /= 2;    
+        }
+        if (other.tag == "SpawnPoint")
+        {
+            spawnPoint = other.transform;
+        }
+    }
+    private void OnCollissionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Damage"))
+        {
+            DamagePlayer(10);
+        }
+    }
+}
