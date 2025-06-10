2c2bfeab90876a694d219a3836fcd17cc1c4c797
FPG1THUB <elijahmcgowan008@Outlook.com>
2025-06-10 10:18:04 +0800

    Added Keybind maniger
    Added Keybind Manager script

--- RPG_Game/Assets/Scripts/Eli/KeybindManager.cs
+++ RPG_Game/Assets/Scripts/Eli/KeybindManager.cs
@@ -1,16 +1,51 @@
 using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections.Generic;
 
 public class KeybindManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Dictionary<string, KeyCode> keys = new Dictionary<string, KeyCode>();
+
+    public void SetUpLoadedKey(string[] key, string[] value)
     {
-        
+        keys.Clear();
+
+        for (int i = 0; i < key.Length; i++)
+        {
+            keyCode parsedKey = (KeyCode)Enum.Parse(typeof(KeyCode), value[i]);
+            keys.Add(key[i], parsedKey);
+        }
     }
 
+
+
     // Update is called once per frame
-    void Update()
+    public string[] SendKey()
     {
-        
+        string[] tempKey = new string[keys.Count];
+
+        int i = 0;
+        foreach (keyValuePair<string, keyCode> key in keys)
+        {
+            tempKey[i] = key.key;
+            i++;
+        }
+        return tempKey;
+    }
+
+
+    public string[] SendValue()
+    {
+        string[] tempValue = new string[keys.count];
+
+        int i = 0;
+        foreach (keyValuePair<string, keyCode> key in keys)
+        {
+            tempValue[i] = key.Value.ToString();
+
+            i++;
+        }
+        return tempValue;
     }
 }
