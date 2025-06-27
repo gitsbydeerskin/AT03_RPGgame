bd7a882ce302266d1209b567ca0f51752cb54ebb
gitsbydeerskin <119915520+gitsbydeerskin@users.noreply.github.com>
2025-06-27 14:12:40 +0800

    refactor: Code comments
    No edits, no changes, just commented all my code.

--- RPG_Game/Assets/Scripts/Bri/StatsLevelling/PlayerStats.cs
+++ RPG_Game/Assets/Scripts/Bri/StatsLevelling/PlayerStats.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 
 [Serializable]
 public class PlayerStats
+    // These are the playerstats. Not all are used. Most are just placeholders. 
 {
     [Header("Character Name")]
     public string name;
@@ -13,20 +14,29 @@ public class PlayerStats
     [Header("Character Level")]
     public int level;
     public Attribute experience;
+    // The 3 Attributes (excl. Level) are used by the Player in the game every second. Stamina is used for Sprinting. Health is used for surviving Damage.
+    // Level is being tied INTO the other two Attributes and the Stats below.
+    // Stats are different than Attributes.
+    // Stats are more like 'Skills' - they effect the 3 base Attributes and other gameplay mechanics in unique ways. Like impacting movement speed and jump height, etc. Or how nice a character speaks to you. 
+
     [Header("Character Stats")]
     public List<Stat> stats = new List<Stat>
     {
-    new Stat { name = "STR", statValue = 12, value = 0 },
-    new Stat { name = "DEX", statValue = 14, value = 0 },
-    new Stat { name = "CON", statValue = 13, value = 0 },
-    new Stat { name = "INT", statValue = 10, value = 0 },
-    new Stat { name = "WIS", statValue = 11, value = 0 },
-    new Stat { name = "CHA", statValue = 8, value = 0 },
+        // Rather than typing out the full words, I opted for using the first 3 letters of each skill.
+        // strength is STR. Intelligence is INT. Charisma is CHA. Etc. 
+        new Stat { name = "STR", statValue = 12, value = 0 },
+        new Stat { name = "DEX", statValue = 14, value = 0 },
+        new Stat { name = "CON", statValue = 13, value = 0 },
+        new Stat { name = "INT", statValue = 10, value = 0 },
+        new Stat { name = "WIS", statValue = 11, value = 0 },
+        new Stat { name = "CHA", statValue = 8, value = 0 },
     };
 }
 
 [Serializable]
 public struct Attribute
+    // This is the Struct for the ATTRIBUTES
+    // Health, Stamina, and Level
 {
     public float currentValue;
     public float maxValue;
@@ -35,6 +45,8 @@ public struct Attribute
 
 [Serializable]
 public struct Stat
+    // This is the Struct for the STATS
+    // STR, Dex, etc. 
 {
     public string name;
     public int statValue;
--- RPG_Game/Assets/Scripts/Bri/StatsLevelling/UIManager.cs
+++ RPG_Game/Assets/Scripts/Bri/StatsLevelling/UIManager.cs
@@ -7,9 +7,13 @@ public class UIManager : MonoBehaviour
     public Image healthBar;
     public Image staminaBar;
     public Image experienceBar;
+    // These are the 3 main UI elements that will update in real-time based on Player input. 
+    // They will be called to and manipulated by PlayerHandler.cs
 
     public void UpdateUI(Image displayImage, float currentValue, float maxValue)
     { 
         displayImage.fillAmount = Mathf.Clamp01(currentValue / maxValue);
+        // This clamps the three UI bars between the Player's current value - ie, if they sprint and drain some Stamina, that would be their current value - and their MAX value. 
+        // This way the UI bars will not 'spill over' or show more just because we're actually using maths to for the Player's stats. 
     }
 }
